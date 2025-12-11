7ab61b5e4fb128c1cff17380c921e6dc77223f38
Oashi <sizanis30@gmail.com>
2025-12-11 21:30:48 +0600

    add navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,40 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './components/Home.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+
+
+
+const router = createBrowserRouter([
+
+{
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home/>,
+      },
+      {
+        path: "/foods",
+        element: <FoodList />,
+      },
+      
+    ],
+  },
+  
+
+
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+    <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
